1a3d32426d2e737137d598c3bb8ef5f9a9348478
Roman <odyssei87@gmail.com>
2023-04-10 21:26:00 +0400

    Добавьте файлы проекта.

---
+++ DZ_Loger_Form/FileLogers.cs
@@ -0,0 +1,29 @@
+﻿namespace DZ_Loger_Form
+{
+    public class FileLogers : ILogger, IDisposable
+    {
+        string filePath;
+        static object _lock = new object();
+        public FileLogers(string path)
+        {
+            filePath = path;
+        }
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return this;
+        }
+        public void Dispose() { }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            lock (_lock)
+            {
+                File.AppendAllText(filePath, formatter(state, exception) + Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ DZ_Loger_Form/FileLoggerProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace DZ_Loger_Form
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        string path;
+        public FileLoggerProvider(string path)
+        {
+            this.path = path;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogers(path);
+        }
+
+        public void Dispose() { }
+    }
+}
